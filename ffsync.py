a615ed2f5d310beaac406435eed9e953439e5cc7
Florent Flament <contact@florentflament.com>
2024-04-23 10:18:36 +0200

    Handle errors

--- ffsync.py
+++ ffsync.py
@@ -29,7 +29,7 @@ def process_yaml(filename):
             # Don't do anything if no roms specified for the title
         return rv
 
-def find_roms(data, roms_path):
+def find_roms(data, roms_dir):
     """data is a list of dictionaries containing a rom_name field.
     The function will add a rom_path field.
 
@@ -37,69 +37,85 @@ def find_roms(data, roms_path):
     # First index our structure by rom_name
     data_h = {rom_data["rom_name"]: rom_data for rom_data in data}
 
-    for dirpath, dirnames, filenames in os.walk(roms_path):
+    for dirpath, dirnames, filenames in os.walk(roms_dir, followlinks=True):
         for f in filenames:
             if f in data_h.keys():
                 data_h[f]["rom_path"] = os.path.join(dirpath, f)
     return data
 
-def copy_roms(data, dest_dir):
-    dir_files = os.listdir(dest_dir)
+def copy_roms(data, target_dir):
+    dir_files = os.listdir(target_dir)
 
     for e in data:
         source = e["rom_path"]
         fname = e["file_name"]
-        dest = os.path.join(dest_dir, fname)
+        target = os.path.join(target_dir, fname)
         prefix = e["file_name"].split("_")[0]
 
-        if (os.path.exists(dest)):
+        if (os.path.exists(target)):
             print(f"{fname} [skipping] Already there")
             continue
 
         for f in dir_files:
             if f.startswith(prefix):
                 print(f"{f} [removing] Matching prefix")
-                os.remove(os.path.join(dest_dir, f))
+                os.remove(os.path.join(target_dir, f))
 
         print(f"{e['file_name']} <- {source}")
-        shutil.copy(source, dest)
-        os.chmod(dest, 0o444)
+        shutil.copy(source, target)
+        os.chmod(target, 0o444) # Set files in read-only mode
 
-def process_prune(data, dest_dir):
-    target_files = os.listdir(dest_dir)
+def process_prune(data, target_dir):
+    target_files = os.listdir(target_dir)
     specif_files = set([e["file_name"] for e in data])
     for tf in target_files:
         if tf.startswith("DSKA") and tf not in specif_files:
             print(f"{tf} [pruning]")
-            os.remove(os.path.join(dest_dir, tf))
+            os.remove(os.path.join(target_dir, tf))
+
+def check_rom_paths(data):
+    return [e["rom_name"] for e in data if "rom_path" not in e]
 
 @click.command()
-@click.option("--config-file", required=True,
+@click.option("--config-file", default="./ffsync.yaml",
               help="The YAML configuration file specifying the roms to synchronize.")
-@click.option("--roms-path", required=False,
+@click.option("--roms-dir", default=".",
               help="The path to the TOSEC directory where the roms are to be copied from. Defaults to \".\".")
-@click.option("--dest-dir", required=True,
+@click.option("--target-dir", required=True,
               help="The target directory; typically a mounted USB key to be inserted into a Gotek drive flashed with flashfloppy.")
-@click.option("--prune", required=False,
+@click.option("--prune/--no-prune",
               help="Remove DSKA* files not specified in the configuration file. Defaults to False.")
-def main(config_file, roms_path, dest_dir, prune):
-    """Copy roms to dest_dir according to specifications provided in
-    the configuration file.
+def main(config_file, roms_dir, target_dir, prune):
+    """Copy roms to the target directory in accordance with the
+    configuration file.
 
     """
     print(f"""Using parameters:
 config-file: {config_file}
-roms-path: {roms_path}
-dest-dir: {dest_dir}
+roms-dir: {roms_dir}
+target-dir: {target_dir}
 """)
-    data = process_yaml(config_file)
-    search_paths = ["."]
-    if roms_path:
-        search_paths.append(roms_path)
-    for path in search_paths:
-        find_roms(data, path)
-    copy_roms(data, dest_dir)
-    if prune: process_prune(data, dest_dir)
+    try:
+        data = process_yaml(config_file)
+    except FileNotFoundError as e:
+        print("[Error] while processing configuration file.")
+        print(e)
+        return
+
+    roms_not_found = check_rom_paths(find_roms(data, roms_dir))
+    if roms_not_found:
+        print("[Error] Some roms could not be found:")
+        for r in roms_not_found: print(r)
+        return
+
+    try:
+        copy_roms(data, target_dir)
+    except FileNotFoundError as e:
+        print("[Error] while copying roms.")
+        print(e)
+        return
+
+    if prune: process_prune(data, target_dir)
 
 if __name__ == "__main__":
-    main(auto_envvar_prefix='TOSYNC')
+    main(auto_envvar_prefix='FFSYNC')
